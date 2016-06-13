465b1e82a354978491f80077a80f5c8ee698cc1b
openboo <openbook@sigaint.org>
2016-06-13 23:42:19 +0000

    Update about.php
    Removed unnecessary bootstrap stylesheets and glyph-icon stuff at the top: It was broken without javascript anyways. Also removed unnecessary javascript stuff at the bottom if there was any. The mail icon has been replaced with an actual image.

--- about.php
+++ about.php
@@ -9,12 +9,11 @@
 		<meta charset="utf-8">
 		<title>openbook</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<meta name="author" content="">
+		<meta name="description" content="An Anonymous hive mind social media network website. One p0rtal among #thep0rtals.">
+		<meta name="author" content="Anonymous">
 
 		<!-- Styles for Bootstrap -->
 		<link href="css/bootstrap.css" rel="stylesheet">
-		<link href="css/bootstrap-responsive.css" rel="stylesheet">
 		
 		<!-- Extra Stylin' -->
 		<link href="css/style.css" rel="stylesheet">
@@ -23,13 +22,6 @@
 		<!--[if lt IE 9]>
 			<script src="../assets/js/html5shiv.js"></script>
 		<![endif]-->
-
-		<!-- Fav and touch icons -->
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-		<link rel="shortcut icon" href="ico/favicon.png">
 
 		<!-- Simple Ajax Library (SAL) -->
 		<script language="javascript" src="js/ajax.js"></script>
@@ -112,7 +104,7 @@
 <?php
 
 	/** Updates mail icon to alert user of how many posts in the users 'watch list' have had comments added. **/
-	if(count($watching)>0){
+	if(count($watching)>0 1==1){
 		$updates = 0;
 		//check for updates for each postid,timestamp pair in $watching
 		foreach($watching as $postidkey=>$lastactivitytimestamp){
@@ -128,11 +120,12 @@
 			}
 		}
 		//if there are updates, show the mail icon, and how many updates there are.
+		$updates = 3;
 		if($updates>0){
 
 ?>
 						<li>
-							<a href="updates.php"><span style="display: inline;" class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;<?php echo $updates; ?></a>
+							<a href="updates.php"><span style="display: inline;"><img src="mailicon.png" /></span>&nbsp;&nbsp;<?php echo $updates; ?></a>
 						</li>
 <?php
 
@@ -376,9 +369,9 @@
 						Copycat websites are welcome, as diversity is in nature itself and Anonymous
 						is only powerful with multiplicity and decentralization.
 						<br/><br/>
-						Openbook Opensource Software is licensed under the
+						Openbook Opensource Software is copylefted under the
 						<a href="LICENSE">Affero GPLv3</a> which means any and all modifications made to the website must be open-source as well, and
-						any website based on this software must provide a link to it's own source-code. <a href="https://github.com/openboo/Openbook-Opensource-Software">Ours is unedited</a>.
+						any website based on this software must provide a link to it's own source-code: <a href="https://github.com/openboo/Openbook-Opensource-Software">So here is this p0rtal's source code</a>.
 						<br/><br/>
 						This website was created in the spirit of <a href="http://distribution.neocities.org">distribution</a>.
 					</span>
@@ -394,23 +387,6 @@
 			</div>
  
 		</div><!-- /container -->
-
-		<!-- javascript
-		================================================== -->
-		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="js/jquery.js"></script>
-		<script src="js/bootstrap-transition.js"></script>
-		<script src="js/bootstrap-alert.js"></script>
-		<script src="js/bootstrap-modal.js"></script>
-		<script src="js/bootstrap-dropdown.js"></script>
-		<script src="js/bootstrap-scrollspy.js"></script>
-		<script src="js/bootstrap-tab.js"></script>
-		<script src="js/bootstrap-tooltip.js"></script>
-		<script src="js/bootstrap-popover.js"></script>
-		<script src="js/bootstrap-button.js"></script>
-		<script src="js/bootstrap-collapse.js"></script>
-		<script src="js/bootstrap-carousel.js"></script>
-		<script src="js/bootstrap-typeahead.js"></script>
 
 	</body>
 </html>
