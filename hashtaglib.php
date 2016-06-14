9da9ecf06a0c65a9f1de8d30001dce5671da901b
openboo <openbook@sigaint.org>
2016-06-14 01:55:25 +0000

    Create hashtaglib.php
    Finally utilizing "include()" to compartmentalize the code better.

---
+++ hashtaglib.php
@@ -0,0 +1,36 @@
+<?php
+	/** Hashtags Library **/
+
+	/* Get a printable array of hashtags from a string. */
+	function get_hashtags($string, $str = 1) {
+		preg_match_all('/#(\w+)/',$string,$matches);
+		$i = 0;
+		$keywords = [];
+		if($str){
+			foreach ($matches[1] as $match){
+				$count = count($matches[1]);
+				$keywords .= "$match";
+				$i++;
+				if ($count > $i) $keywords .= ", ";
+			}
+		}else{
+			foreach ($matches[1] as $match){
+				$keyword[] = $match;
+				$keywords = $keyword;
+			}
+		}
+		return $keywords;
+	}
+
+	/* Convert a string so that all hashtags are turned into pre-formatted links (as defined in the function above).*/
+	function hashtag_links($string) {
+		//Find all matches to character strings that start with '#'.
+		preg_match_all('/#(\w+)/',$string,$matches);
+		//Convert each one into a pre-formatted link by surrounding it with the <a> tags.
+		foreach ($matches[1] as $match) {
+			//The way we use it here is just to link to the search page, where the search is for the hashtag.
+			$string = str_replace("#$match", "<a href='search.php?search=%23$match'>#$match</a>", "$string");
+		}
+		return $string;//Returns the newly formatted link-filled string, as ordered.
+	}
+?>
