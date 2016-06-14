271be8beab06eb66c3b1ef41ba32ef74d136da82
openboo <openbook@sigaint.org>
2016-06-14 02:03:44 +0000

    Update search.php
    Finally utilizing "include()" to compartmentalize the code better.

--- search.php
+++ search.php
@@ -1,122 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-	<head>
-		<!--You've uncovered another stop on the underground railroad!-->
-		<!--Perhaps you're looking for: p0rtalurl/IMPORTANT.txt -->
-
-		<!-- Metadata -->
-		<meta charset="utf-8">
-		<title>openbook</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="An Anonymous hive mind social media network website. One p0rtal among #thep0rtals.">
-		<meta name="author" content="Anonymous">
-
-		<!-- Styles for Bootstrap -->
-		<link href="css/bootstrap.css" rel="stylesheet">
-
-		<!-- Extra Stylin' -->
-		<link href="css/style.css" rel="stylesheet">
-
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-		<!--[if lt IE 9]>
-			<script src="../assets/js/html5shiv.js"></script>
-		<![endif]-->
-
-		<!-- Simple Ajax Library (SAL) -->
-		<script language="javascript" src="js/ajax.js"></script>
-
-	</head>
-
-	<body>
-
-<?php
-
-	/** Hashtags Library **/
-
-	/* Get a printable array of hashtags from a string. */
-	function get_hashtags($string, $str = 1) {
-		preg_match_all('/#(\w+)/',$string,$matches);
-		$i = 0;
-		$keywords = [];
-		if($str){
-			foreach ($matches[1] as $match){
-				$count = count($matches[1]);
-				$keywords .= "$match";
-				$i++;
-				if ($count > $i) $keywords .= ", ";
-			}
-		}else{
-			foreach ($matches[1] as $match){
-				$keyword[] = $match;
-				$keywords = $keyword;
-			}
-		}
-		return $keywords;
-	}
-
-	/* Convert a string so that all hashtags are turned into pre-formatted links (as defined in the function above).*/
-	function hashtag_links($string) {
-		//Find all matches to character strings that start with '#'.
-		preg_match_all('/#(\w+)/',$string,$matches);
-		//Convert each one into a pre-formatted link by surrounding it with the <a> tags.
-		foreach ($matches[1] as $match) {
-			//The way we use it here is just to link to the search page, where the search is for the hashtag.
-			$string = str_replace("#$match", "<a href='search.php?search=%23$match'>#$match</a>", "$string");
-		}
-		return $string;//Returns the newly formatted link-filled string, as ordered.
-	}
-
-
-
-	/** Set up Database **/
-	//connect or die with error message.
-	$conn = mysqli_connect("serveraddress","SQLuser","databasepassword","databasename");
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-
-
-
-	/** Set up Watch List **/
-	/* The watch list keeps track of which posts we posted or commented on, and the last time we viewed them.
-	 * This list is stored in our cookies, as pairings of a post id, and the timestamp we last viewed that post.
-	 * This list is then used to alert the user if there are any new comments on any posts in their watch list.
-	 * It does this by comparing the last time you viewed a post with the timestamp on it's most recent comment. */
-	//This is the variable used to interact with the watch list, representing the watch list in our cookies.
-	$watching = array();
-	//Check if the watch list cookie is set.
-	if(isset($_COOKIE['watching'])){
-		//If so, set our watching variable to match the actual watch list in the cookies.
-		$cookie = stripslashes($_COOKIE['watching']);
-		//update watching var
-		$watching = json_decode($cookie,true);//decode cookie
-	}
-
-
-
-	/** Big Brother Alert **/
-	$off = "";
-	if(isset($_GET['off'])){
-		$off = $_GET['off'];
-		if($off=="bigbrotheralert"){
-			setcookie("bigbrotheralert","off");
-		}
-	}elseif(!isset($_COOKIE['bigbrotheralert'])){
-
-
-?>
-		<div id="alert">
-			<a href="?off=bigbrotheralert" style="float: right;"><img id="closebutton" style="border: 0px;" src="closebutton.png"></a>
-			Browser activity and IP addresses are being logged by government surveillance.
-			<br/>
-			Anonymity is won only by understanding and using multiple tools like <a href="http://lifehacker.com/what-is-tor-and-should-i-use-it-1527891029">Tor-Browser</a>.
-		</div>
-<?php
-
-	}
-
-?>
+<?php include("header.php");?>
+<?php include("hashtaglib.php");?>
+<?php include("databasesetup.php");//this gives us $conn to connect to mysql.?>
+<?php include("watchlist.php");?>
 		<!-- Page Container -->
 		<div class="container-narrow">
 
@@ -487,14 +372,9 @@
 			<script>
 				document.getElementById("loadmorebutton").href="#load<?php echo $lasttimestamp;?>";
 			</script>
-
 <?php
-
     }
-
 ?>
 		</div><!-- /container -->
-
 	</body>
-
 </html>
