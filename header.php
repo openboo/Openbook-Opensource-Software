f455688bf05ea1c075d83af771865ea03656630c
openboo <openbook@sigaint.org>
2016-06-14 01:54:15 +0000

    Create header.php
    Finally utilizing "include()" to compartmentalize the code better.

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<!--You've uncovered another stop on the underground railroad!-->
+		<!--Perhaps you're looking for: p0rtalurl/IMPORTANT.txt -->
+
+		<!-- Metadata -->
+		<meta charset="utf-8">
+		<title>openbook</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="An Anonymous hive mind social media network website. One p0rtal among #thep0rtals.">
+		<meta name="author" content="Anonymous">
+
+		<!-- Styles for Bootstrap -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+
+		<!-- Extra Stylin' -->
+		<link href="css/style.css" rel="stylesheet">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+			<script src="../assets/js/html5shiv.js"></script>
+		<![endif]-->
+
+		<!-- Simple Ajax Library (SAL) -->
+		<script language="javascript" src="js/ajax.js"></script>
+
+	</head>
+	<body>
+<?php include("bigbrotheralert.php");//the only thing about including it here is that this alert is always above any other alerts.?>
