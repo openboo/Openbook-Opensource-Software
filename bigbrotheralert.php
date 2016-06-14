9d08eaecd17081386073f05359d1de41fb80813c
openboo <openbook@sigaint.org>
2016-06-14 01:53:02 +0000

    Create bigbrotheralert.php
    Finally utilizing "include()" to compartmentalize the code better.

---
+++ bigbrotheralert.php
@@ -0,0 +1,19 @@
+<?php
+		/** Big Brother Alert **/
+		$off = "";
+		if(isset($_GET['off'])){
+			$off = $_GET['off'];
+			if($off=="bigbrotheralert"){
+				setcookie("bigbrotheralert","off");
+			}
+		}elseif(!isset($_COOKIE['bigbrotheralert'])){
+?>
+		<div id="alert">
+			<a href="?off=bigbrotheralert" style="float: right;"><img id="closebutton" style="border: 0px;" src="closebutton.png"></a>
+			Browser activity and IP addresses are being logged by government surveillance.
+			<br/>
+			Anonymity is won only by understanding and using multiple tools like <a href="http://lifehacker.com/what-is-tor-and-should-i-use-it-1527891029">Tor-Browser</a>.
+		</div>
+<?php
+		}
+?>
