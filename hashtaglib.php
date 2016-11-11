9ef82a8ca2ceb4c5cf62f4f92010e1b65fedaaed
Anonymous <openbook@sigaint.org>
2016-11-11 14:47:59 -0800

    Update hashtaglib.php
    Added a requirement for a space right before hashtag, since that allows urls to have a # in them without conflict. Also, simulates a space in at the beginning of any string it's asked to assess, as a shortcut to fix the problem of hashtags not being acknowledged if they are at the very beginning of the string.

--- hashtaglib.php
+++ hashtaglib.php
@@ -3,7 +3,9 @@
 
 	/* Get a printable array of hashtags from a string. */
 	function get_hashtags($string, $str = 1) {
-		preg_match_all('/#(\w+)/',$string,$matches);
+		//emulate a space before, so you can start a post with a hashtag
+		$string = " ".$string;
+		preg_match_all('/\s#(\w+)/',$string,$matches);
 		$i = 0;
 		$keywords = [];
 		if($str){
@@ -24,12 +26,14 @@
 
 	/* Convert a string so that all hashtags are turned into pre-formatted links (as defined in the function above).*/
 	function hashtag_links($string) {
+		//emulate a space before, so you can start a post with a hashtag
+		$string = " ".$string;
 		//Find all matches to character strings that start with '#'.
-		preg_match_all('/#(\w+)/',$string,$matches);
+		preg_match_all('/\s#(\w+)/',$string,$matches);
 		//Convert each one into a pre-formatted link by surrounding it with the <a> tags.
 		foreach ($matches[1] as $match) {
 			//The way we use it here is just to link to the search page, where the search is for the hashtag.
-			$string = str_replace("#$match", "<a href='search.php?search=%23$match'>#$match</a>", "$string");
+			$string = str_replace(" #$match", " <a href='search.php?search=%23$match'>#$match</a>", "$string");
 		}
 		return $string;//Returns the newly formatted link-filled string, as ordered.
 	}
