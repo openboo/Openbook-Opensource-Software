9441d7a0d0495eb262aa986a7eececd3d988a8e1
Anonymous <openbook@sigaint.org>
2016-11-18 20:57:44 -0800

    Update databasesetup.php
    Organized settings and added settings for easily changing how many profile pictures to randomly select from and what file extension they all have (has to all be the same file extension)

--- databasesetup.php
+++ databasesetup.php
@@ -1,7 +1,19 @@
 <?php
+
+        /** Settings **/
+        $settings = array();
+        $settings['serveraddress'] = 'localhost';//may change
+        $settings['SQLuser'] = '';//fill in
+        $settings['SQLpassword'] = '';//fill in
+        $settings['databasename'] = '';//fill in
+
+        /* The number of profile images to randomly select from. */
+        $settings['numimages'] = 47;//Named anon1.jpg through anon*.jpg
+        $settings['imagetype'] = "jpg";//or anon1.png through anon*.png for example.
+
 	/** Set up Database **/
 	//connect or die with error message.
-	$conn = mysqli_connect("serveraddress","SQLuser","databasepassword","databasename");
+	$conn = mysqli_connect($settings['serveraddress'],$settings['SQLuser'],$settings['SQLpassword'],$settings['databasename']);
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
