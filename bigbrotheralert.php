866f1ba6fb3bc5a9b9a969d27b622bf0e582cb25
Anonymous <openbook@sigaint.org>
2016-11-19 15:14:17 -0800

    Update bigbrotheralert.php
    Made sure the close button link goes to index.php so we don't have errors like on the comments page when the close button is hit it becomes comments.php?off=bigbrotheralert which means no I'd is set so no post is selected which throws an error on the comments page.

--- bigbrotheralert.php
+++ bigbrotheralert.php
@@ -9,7 +9,7 @@
 		}elseif(!isset($_COOKIE['bigbrotheralert'])){
 ?>
 		<div id="alert">
-			<a href="?off=bigbrotheralert" style="float: right;"><img id="closebutton" style="border: 0px;" src="closebutton.png"></a>
+			<a href="index.php?off=bigbrotheralert" style="float: right;"><img id="closebutton" style="border: 0px;" src="closebutton.png"></a>
 			Browser activity and IP addresses are <br class="rwd-break" />being logged by government surveillance.
 			<br/><br class="rwd-break" />
 			Anonymity is won only by understanding <br class="rwd-break" />and using multiple tools like <a href="http://lifehacker.com/what-is-tor-and-should-i-use-it-1527891029">Tor-Browser</a>.
