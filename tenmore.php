b0e40427afa6595f374b5b2a7cb10eb4207c2c99
Anonymous <openbook@sigaint.org>
2016-11-26 17:28:05 -0800

    Update tenmore.php
    Added 'numimages' and 'imagetype' settings.

--- tenmore.php
+++ tenmore.php
@@ -40,9 +40,6 @@
 		}
 
 
-
-
-
     //ASSUMES "?sort=recent"
 	if($sortby=="popular"){
 		//For 'popular' lastid just counts how many posts have been displayed
@@ -73,10 +70,10 @@
 	<!-- Status Update -->
 	<div class="container-fluid">
 		<div class="row-fluid post">
-          
+
 			<!-- User Identity -->
 			<div class="col-sm-2 col-xs-3" style="1px solid black; margin: 0px; padding: 0px;">
-				<img src="anons/anon<?php echo(rand(1,47)); ?>.jpg" />
+				<img src="anons/anon<?php echo(rand(1,$settings['numimages']).".".$settings['imagetype']);?>" />
 				Anonymous
 			</div>
 
